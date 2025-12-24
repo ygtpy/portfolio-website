0346e7f646742ca11772c504b94ed3b445c10646
yigitPy <ygt.developer03py@gmail.com>
2025-12-24 20:12:29 +0300

    feat: admin paneli ve skill yönetimi eklendi

---
+++ Controllers/SkillController.cs
@@ -0,0 +1,76 @@
+﻿using AkademiQPortfolio.Data;
+using AkademiQPortfolio.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQPortfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SkillController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult SkillList()
+        {
+            var values = _context.Skills.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSkill(Skill skill)
+        {
+            _context.Skills.Add(skill);
+            _context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public IActionResult DeleteSkill(int id)
+        {
+            var skill = _context.Skills.Find(id);
+
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            _context.Skills.Remove(skill);
+            _context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSkill(int id)
+        {
+            var skill = _context.Skills.Find(id);
+
+            if (skill == null)
+            {
+                return NotFound();
+            }
+            return View(skill);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public IActionResult UpdateSkill(Skill skill)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(skill);
+            }
+
+            _context.Skills.Update(skill);
+            _context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+    }
+}
---
+++ ViewComponents/AdminLayoutViewComponents/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQPortfolio.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
